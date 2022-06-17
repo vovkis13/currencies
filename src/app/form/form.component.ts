20c6d0df4987e062789c4c9379bc3c6e60a8a3a9
Volodymyr Klochan <vovkis13@gmail.com>
2022-06-17 16:04:55 +0300

    code optimized

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -25,15 +25,7 @@ export class FormComponent implements OnInit {
   options: Currency[] = [Currency.UAH, Currency.USD, Currency.EUR];
   currencies: [Currency, Currency] = [Currency.UAH, Currency.USD];
   values: [number, number] = [0, 0];
-
-  rates: RatesElement[] = [
-    { pair: 'UAHUSD', rate: 0 },
-    { pair: 'UAHEUR', rate: 0 },
-    { pair: 'USDUAH', rate: 0 },
-    { pair: 'EURUAH', rate: 0 },
-    { pair: 'USDEUR', rate: 0 },
-    { pair: 'EURUSD', rate: 0 },
-  ];
+  rates: RatesElement[] = [];
 
   constructor(
     private getCurrenciesService: GetCurrenciesService,
@@ -41,45 +33,41 @@ export class FormComponent implements OnInit {
   ) {}
 
   handleValueChange(changedValueNumber: number): void {
-    if (this.values[0] < 0 || this.values[1] < 0) {
-      this.values.fill(0);
-      return;
+    if (this.values.some((value) => value < 0)) this.values.fill(0);
+    else {
+      const exchangeRate = this.rates.find(
+        (ratesElement: RatesElement) =>
+          ratesElement.pair === this.currencies.join('')
+      )?.rate as number;
+
+      this.values[1 - changedValueNumber] = this.roundRateService.round(
+        this.values[changedValueNumber] *
+          (changedValueNumber === 0 ? exchangeRate : 1 / exchangeRate)
+      );
     }
-
-    const exchangeRate = this.rates.find(
-      (ratesElement: RatesElement) =>
-        ratesElement.pair === this.currencies.join('')
-    )?.rate as number;
-
-    if (changedValueNumber === 0)
-      this.values[1] = this.roundRateService.round(
-        this.values[0] * exchangeRate
-      );
-    else
-      this.values[0] = this.roundRateService.round(
-        this.values[1] / exchangeRate
-      );
   }
-
-  handleCurrencyChange(changedCurrencyNumber: number) {
-    if (this.currencies[0] === this.currencies[1]) {
+  handleCurrencyChange(changedCurrencyNumber: number): void {
+    if (this.currencies[0] === this.currencies[1])
       this.currencies[1 - changedCurrencyNumber] =
         this.currencies[changedCurrencyNumber] === Currency.UAH
           ? Currency.USD
           : Currency.UAH;
-    }
     this.handleValueChange(changedCurrencyNumber);
+  }
+
+  private fillRates(rates: Rate[]) {
+    this.rates[0] = { pair: 'UAHUSD', rate: 1 / rates[0].sale };
+    this.rates[1] = { pair: 'UAHEUR', rate: 1 / rates[1].sale };
+    this.rates[2] = { pair: 'USDUAH', rate: rates[0].buy };
+    this.rates[3] = { pair: 'EURUAH', rate: rates[1].buy };
+    this.rates[4] = { pair: 'USDEUR', rate: rates[0].buy / rates[1].sale };
+    this.rates[5] = { pair: 'EURUSD', rate: rates[1].buy / rates[0].sale };
   }
 
   ngOnInit() {
     this.getCurrenciesService.getRates().subscribe((rates: Rate[]) => {
       this.loading = false;
-      this.rates[0].rate = 1 / rates[0].sale; //UAHUSD
-      this.rates[1].rate = 1 / rates[1].sale; //UAHEUR
-      this.rates[2].rate = rates[0].buy; //USDUAH
-      this.rates[3].rate = rates[1].buy; //EURUAH
-      this.rates[4].rate = rates[0].buy / rates[1].sale; //USDEUR
-      this.rates[5].rate = rates[1].buy / rates[0].sale; //EURUSD
+      this.fillRates(rates);
     });
   }
 }
